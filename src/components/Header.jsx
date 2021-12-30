44488efab15c89532eb963875590d72e97cd8b14
Gaurav <gauravpawar3102@gmail.com>
2021-12-30 14:06:53 +0530

    nav bar added

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faVirus } from '@fortawesome/free-solid-svg-icons';
+import '../styles/header.css';
+function Header() {
+  return (
+    <div className="container">
+      <div className="logo">
+        <FontAwesomeIcon className="logo-icon" size="2x" icon={faVirus} />
+        <h1 className="Logo-text">Covid-19</h1>
+      </div>
+      <section className="menu">
+        <ul>
+          <li>Home</li>
+          <li>our Team</li>
+          <li>Contact-Us</li>
+        </ul>
+      </section>
+    </div>
+  );
+}
+
+export default Header;
