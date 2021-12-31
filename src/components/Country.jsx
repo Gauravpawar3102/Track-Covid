4d5865bbbdc0a8d41ada6a12c8614a4f0e89b1d2
Gaurav <gauravpawar3102@gmail.com>
2022-01-01 00:57:48 +0530

    Yay completed fetching

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,18 +1,49 @@
-import React, { Component } from 'react';
-// import { countryName } from './CountryData';
-import getData from './CountryData';
-class Country extends Component {
-  render() {
-    return (
-      <div>
-        <select>
-          <option value="global">Global</option>
-          <option value="global">Global</option>
-        </select>
-        {/* <countryData /> */}
-      </div>
-    );
-  }
+import React, { useState } from 'react';
+
+function Country() {
+  const [countries, setCountries] = useState([]);
+  const onChange = (e) => {
+    console.log(e.target.value);
+  };
+  const createSelectItems = (e) => {
+    fetch(
+      'https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/npm-covid-data/',
+      {
+        method: 'GET',
+        headers: {
+          'x-rapidapi-host':
+            'vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com',
+          'x-rapidapi-key':
+            '2b66cc80femshb1df0e1e9c92c59p13ac85jsnfbf1187edcae',
+        },
+      }
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        // console.log(res);
+        setCountries(res);
+      });
+    let items = [];
+    // console.log(data.map((countries) => countries.Country));
+    // console.log(countries);
+    countries.forEach((country) => {
+      items.push(
+        <option key={country.id} value={country.id}>
+          {country.Country}
+        </option>
+      );
+    });
+    // console.log(items);
+    return items;
+  };
+  return (
+    <div>
+      <label htmlFor="countries">select Country</label>
+      <select onChange={onChange}>{createSelectItems()}</select>
+    </div>
+  );
 }
 
 export default Country;
--- src/components/CountryData.js
+++ src/components/CountryData.js
@@ -1,25 +1,25 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-const countryData = {
-  method: 'GET',
-  url: 'https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/npm-covid-data/',
-  headers: {
-    'x-rapidapi-host':
-      'vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com',
-    'x-rapidapi-key': '2b66cc80femshb1df0e1e9c92c59p13ac85jsnfbf1187edcae',
-  },
-};
-console.log('akjsdh');
+// const countryData = {
+//   method: 'GET',
+//   url: 'https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/npm-covid-data/',
+//   headers: {
+//     'x-rapidapi-host':
+//       'vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com',
+//     'x-rapidapi-key': '2b66cc80femshb1df0e1e9c92c59p13ac85jsnfbf1187edcae',
+//   },
+// };
+// console.log('akjsdh');
 
-axios
-  .request(countryData)
-  .then(function (response) {
-    // console.log(response.data.map((Countries) => Countries.Country));
-    const data = response.data.map((Countries) => Countries.Country);
-    return data;
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
+// axios
+//   .request(countryData)
+//   .then(function (response) {
+//     // console.log(response.data.map((Countries) => Countries.Country));
+//     const data = response.data.map((Countries) => Countries.Country);
+//     return data;
+//   })
+//   .catch(function (error) {
+//     console.error(error);
+//   });
 
-export default countryData;
+// export default countryData;
