4337d4c870c4733f91298b1adc61210c18280de4
Gaurav <gauravpawar3102@gmail.com>
2022-01-02 00:42:42 +0530

    removed comments

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -40,58 +40,10 @@ function Country() {
     </div>
   );
 }
+export default Country;
+
 //  {
 //    countries.map((country) => (
 //      <option key={country.id}>{country.Country}</option>
 //    ));
 //  }
-export default Country;
-
-//before //
-// function Country() {
-//   const [countries, setCountries] = useState([]);
-//   const onChange = (e) => {
-//     console.log(e.target.value);
-//   };
-//   const createSelectItems = (e) => {
-//     fetch(
-//       'https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/npm-covid-data/',
-//       {
-//         method: 'GET',
-//         headers: {
-//           'x-rapidapi-host':
-//             'vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com',
-//           'x-rapidapi-key':
-//             '2b66cc80femshb1df0e1e9c92c59p13ac85jsnfbf1187edcae',
-//         },
-//       }
-//     )
-//       .then((res) => {
-//         return res.json();
-//       })
-//       .then((res) => {
-//         // console.log(res);
-//         setCountries(res);
-//       });
-//     const items = [];
-//     // console.log(data.map((countries) => countries.Country));
-//     // console.log(countries);
-//     countries.forEach((country) => {
-//       items.push(
-//         <option key={country.id} value={country.id}>
-//           {country.Country}
-//         </option>
-//       );
-//     });
-//     // console.log(items);
-//     return items;
-//   };
-//   return (
-//     <div>
-//       <label htmlFor="countries">select Country</label>
-//       <select onChange={onChange}>{createSelectItems}</select>
-//     </div>
-//   );
-// }
-
-// export default Country;
